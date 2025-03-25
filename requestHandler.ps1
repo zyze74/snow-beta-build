351fc18e3695281227aaede81fc8bdad90da0a86
zyze74 <kianwindus2@gmail.com>
2025-03-25 01:31:50 +0000

    Updated to 0.34.1

--- requestHandler.ps1
+++ requestHandler.ps1
@@ -1,17 +1,29 @@
 $hookUrl = $env:DISCORD_WEBHOOK_URL
 
+$Host.UI.RawUI.ForegroundColor = 'Blue'
+
+Clear-Host
+
 if ([string]::IsNullOrEmpty($hookUrl)) {
+    $Host.UI.RawUI.ForegroundColor = 'Red'
     Write-Output "Webhook URL: Invalid"
+    Write-Output ""
     Write-Output "Error: Webhook URL is not set."
     exit
 } else {
+    $Host.UI.RawUI.ForegroundColor = 'Green'
     Write-Output "Webhook URL: Valid"
+    $Host.UI.RawUI.ForegroundColor = 'Blue'
+    Write-Output ""
+    Write-Output 'Start typing and enter your Feeback:'
 }
 
-Write-Output 'Enter your request below'
-$message = Read-Host ':'
+$message = Read-Host '# '
 $payload = [PSCustomObject]@{content = $message}
 Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json'
-Write-Output 'Request sent. You can now close this window.'
-exit;
 
+Clear-Host
+
+Write-Output 'Feedback submitted. You can now close this window.'
+exit;
+# created by @zyze74
